be7c8987a74530858b2a172be53c0b2adfc08a9c
Jacob Diaz <hello@jacobdiaz.dev>
2021-01-22 22:18:28 -0600

    Completed Project

--- src/Joke.js
+++ src/Joke.js
@@ -3,6 +3,39 @@ import "./JokeList.css";
 import { FaArrowUp, FaArrowDown } from "react-icons/fa";
 
 export default class Joke extends Component {
+  // Picks the border color for the number of votes
+  getColor() {
+    switch (true) {
+      case this.props.votes >= 15:
+        return "#4CAF50";
+      case this.props.votes >= 12:
+        return "#8BC34A";
+      case this.props.votes >= 9:
+        return "#CDDC39";
+      case this.props.votes >= 3:
+        return "#FFEB3B";
+      case this.props.votes >= 0:
+        return "#FFC107";
+    }
+  }
+  // Picks the emoji for number of votes
+  getEmoji() {
+    switch (true) {
+      case this.props.votes >= 15:
+        return <i className="em em-rolling_on_the_floor_laughing"></i>;
+      case this.props.votes >= 12:
+        return <i className="em em-laughing"></i>;
+      case this.props.votes >= 9:
+        return <i className="em em-smiley"></i>;
+      case this.props.votes >= 3:
+        return <i className="em em-slightly_smiling_face"></i>;
+      case this.props.votes >= 0:
+        return <i className="em em-confused"></i>;
+      default:
+        return <i className="em em-angry"></i>;
+    }
+  }
+
   render() {
     return (
       <div className="joke-item">
@@ -10,13 +43,16 @@ export default class Joke extends Component {
         <div className="joke-btn">
           <FaArrowUp onClick={this.props.upVote} className="arrow" />
         </div>
-        <span className="votes">{this.props.votes}</span>
+        <span className="votes" style={{ borderColor: this.getColor() }}>
+          {this.props.votes}{" "}
+        </span>
         <div className="joke-btn">
           <FaArrowDown onClick={this.props.downVote} className="arrow" />
         </div>
 
         {/* Joke Text */}
         <div className="joke-text">{this.props.jokeText}</div>
+        <div className="joke-emoji">{this.getEmoji()}</div>
       </div>
     );
   }
--- src/JokeList.js
+++ src/JokeList.js
@@ -14,14 +14,23 @@ export default class JokeList extends Component {
     super(props);
 
     this.state = {
-      jokes: [],
+      // If there are jokes in the local storage just load those OR parse an empty string
+      jokes: JSON.parse(window.localStorage.getItem("jokes") || "[]"),
     };
     this.handleVote = this.handleVote.bind(this);
+    this.handleClick = this.handleClick.bind(this);
   }
 
-  async componentDidMount() {
-    let fetchedJokes = [];
+  componentDidMount() {
+    // If there aren't any jokes in state get jokes
+    if (this.state.jokes.length === 0) {
+      this.getJokes();
+    }
+  }
 
+  async getJokes() {
+    // Array to hold all the jokes from the api
+    let fetchedJokes = [];
     // Populate an array with jokes from api
     while (fetchedJokes.length < this.props.numJokesToGet) {
       let apiRes = await axios.get(API_URL, {
@@ -29,12 +38,21 @@ export default class JokeList extends Component {
       });
       fetchedJokes.push({ joke: apiRes.data.joke, votes: 0, id: uuid() });
     }
-
-    console.log(fetchedJokes);
     /// Set State to array of jokes that were just fetched
-    this.setState({ jokes: fetchedJokes });
+    this.setState(
+      (st) => ({
+        jokes: [...st.jokes, ...fetchedJokes],
+      }),
+
+      // Saving jokes to Local Storage
+
+      () => {
+        window.localStorage.setItem("jokes", JSON.stringify(this.state.jokes)); // Local storage can only hold strings so we have to stringify the json
+      }
+    );
   }
   handleVote(id, delta) {
+    // Set State
     this.setState((st) => ({
       // map through jokes
       jokes: st.jokes.map(
@@ -43,6 +61,13 @@ export default class JokeList extends Component {
           j.id === id ? { ...j, votes: j.votes + delta } : j // If its not the joke we are looking for we are just returning the joke
       ),
     }));
+
+    // Update Local Storage
+    window.localStorage.setItem("jokes", JSON.stringify(this.state.jokes));
+  }
+
+  handleClick() {
+    this.getJokes();
   }
 
   render() {
@@ -53,11 +78,14 @@ export default class JokeList extends Component {
             <img alt="logo" src={logo} className="logo"></img>
           </div>
           <h1>Dad Jokes</h1>
-          <button className="more-jokes-btn">Get more jokes!</button>
+          <button className="more-jokes-btn" onClick={this.handleClick}>
+            Get more jokes!
+          </button>
         </div>
         <div className="joke-list-rightCol">
           {this.state.jokes.map((j) => (
             <Joke
+              key={j.id}
               jokeText={j.joke}
               votes={j.votes}
               upVote={() => {
